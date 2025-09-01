0774c9f2e0f46302d9dc11f04433bfe3a2592a6f
Uzair Hassan <uzairhassan1357@gmail.com>
2025-09-01 15:27:28 +0500

    changes donee

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.ai.visual.generator.maker"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -27,8 +27,8 @@ android {
         applicationId = "com.ai.visual.generator.maker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23  // Required for Cloud Firestore
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24  // Required for Cloud Firestore
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
